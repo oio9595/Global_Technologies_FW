0d718dc6ac0fcce888c35d94cae1cb17d2e9fee5
JY Lee <oio9595@naver.com>
2026-09-17 10:37:44 +0900

    minor update

--- ID804/ID804_FA_Board/FW/App/User/App/Inc/version.h
+++ ID804/ID804_FA_Board/FW/App/User/App/Inc/version.h
@@ -16,9 +16,9 @@ extern "C" {
 #endif
 
 #define FW_VER_MAJOR    0
-#define FW_VER_MINOR    2
-#define FW_VER_BUILD    56
-#define FW_GIT_REV "08e570b"
+#define FW_VER_MINOR    3
+#define FW_VER_BUILD    7
+#define FW_GIT_REV "2578174-dirty"
 
 #ifdef __cplusplus
 }
